63c4c2bdf775163c5a39be7ff09462983916d163
peterrahme <peter.rahme@ocsbstudent.ca>
2021-02-23 15:36:32 +0000

    spacing

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ let diameter = 0
 const pi = Math.PI
 let radius = 0
 
-// 
+ /* This runs the area and circumference formula for the diameter inputed */ 
 function circle () {
   diameter = document.getElementById('input').value
   radius = diameter / 2
-  area = pi * radius**2
+  area = pi * radius ** 2
   number = 2 * pi * radius
   document.getElementById('answer').innerHTML = number
   document.getElementById('t').innerHTML = area
