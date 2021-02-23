3fe462572b15e1aca5bd3141b9647fdd3a68d494
peterrahme <peter.rahme@ocsbstudent.ca>
2021-02-23 15:12:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// This connects "Calculate circle" button to "circle" function
+document.getElementById('button').addEventListener('click', circle)
+let number = 0
+let area = 0
+let diameter = 0
+const pi = Math.PI
+let radius = 0
+
+// 
+function circle () {
+  diameter = document.getElementById('input').value
+  radius = diameter / 2
+  area = pi * radius**2
+  number = 2 * pi * radius
+  document.getElementById('answer').innerHTML = number
+  document.getElementById('t').innerHTML = area
+}
